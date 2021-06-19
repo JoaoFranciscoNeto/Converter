08405001f3beb407bc483fd6fd3a34979f75d058
João Neto <jneto@mercedesamgf1.com>
2021-06-19 22:30:38 +0100

    Temperature stubs in place

---
+++ Converter.Core.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace Converter.Core.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TemperatureTests
+    {
+        [TestMethod]
+        public void ThatConvertsCelsiusToFahrenheit()
+        {
+        }
+    }
+}
---
+++ Converter.Core/Model/QuantityInfo.cs
@@ -0,0 +1,44 @@
+﻿namespace Converter.Core.Model
+{
+    using System;
+
+    public class Temperature
+    {
+        private readonly double value;
+        private readonly TemperatureUnit unit;
+
+        public Temperature(double value, TemperatureUnit unit)
+        {
+            this.value = value;
+            this.unit = unit;
+        }
+
+        public double As(TemperatureUnit unit)
+        {
+            var baseValue = this.GetBaseValue();
+
+            return this.unit switch
+            {
+                TemperatureUnit.Fahrenheit => 9.0 / 5.0 * (baseValue - 273) + 32,
+                TemperatureUnit.Celsius => baseValue - 273,
+                TemperatureUnit.Kelvin => baseValue,
+                _ => throw new NotImplementedException(),
+            };
+        }
+
+        private double GetBaseValue() => this.unit switch
+        {
+            TemperatureUnit.Kelvin => this.value,
+            TemperatureUnit.Fahrenheit => 5.0 / 9.0 * (this.value - 32.0) + 273.0,
+            TemperatureUnit.Celsius => this.value + 273,
+            _ => throw new ArgumentOutOfRangeException(),
+        };
+    }
+
+    public enum TemperatureUnit
+    {
+        Fahrenheit,
+        Celsius,
+        Kelvin,
+    }
+}
