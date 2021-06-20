799144666fa5fd2a0b6882c242d97b55f6e793eb
João Neto <jneto@mercedesamgf1.com>
2021-06-20 12:35:35 +0100

    Merge client and api

--- api/Controllers/WeatherForecastController.cs
+++ api/Controllers/WeatherForecastController.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using Converter.Core;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
 
@@ -45,14 +44,5 @@
                     })
                 .ToArray();
         }
-    }
-
-    [ApiController]
-    [Route("[controller]")]
-    public class ConverterController : ControllerBase
-    {
-        [HttpGet]
-        public double Get([FromQuery] double value, [FromQuery] string source, [FromQuery] string target) =>
-            UnitConverter.Convert(value, source, target, out var converted) ? converted : double.NaN;
     }
 }
---
+++ client/Controllers/ConverterController.cs
@@ -0,0 +1,14 @@
+﻿namespace client.Controllers
+{
+    using Converter.Core;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ConverterController : ControllerBase
+    {
+        [HttpGet]
+        public double Get([FromQuery] double value, [FromQuery] string source, [FromQuery] string target) =>
+            UnitConverter.Convert(value, source, target, out var converted) ? converted : double.NaN;
+    }
+}
