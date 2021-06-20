fc05b19aefeaddc75423f2841c591d2128b0b403
João Neto <jneto@mercedesamgf1.com>
2021-06-20 04:18:30 +0100

    Add time

---
+++ Converter.Core.Test/TimeTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Converter.Core.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Shouldly;
+
+    [TestClass]
+    public class TimeTests
+    {
+        [TestMethod]
+        public void ThatCanConvertSecondsToMilliseconds()
+        {
+            UnitConverter.Convert(1, "s", "ms", out var converted).ShouldBeTrue();
+            converted.ShouldBe(1000, 0.01);
+        }
+
+        [TestMethod]
+        public void ThatCanConvertMillisecondsToSeconds()
+        {
+            UnitConverter.Convert(1, "ms", "s", out var converted).ShouldBeTrue();
+            converted.ShouldBe(.001, 0.001);
+        }
+    }
+}
--- Converter.Core/Model/UnitInfo.cs
+++ Converter.Core/Model/UnitInfo.cs
@@ -32,5 +32,6 @@
     public enum Quantity
     {
         Temperature,
+        Time,
     }
 }
